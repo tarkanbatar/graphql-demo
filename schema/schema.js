ba744992d8b6a38fddcecc70e2edb78fc5477d60
Tarkan Batar <tarkanbatar4@gmail.com>
2025-10-30 23:46:20 +0300

    Director schema added

--- schema/schema.js
+++ schema/schema.js
@@ -1,6 +1,7 @@
 const graphql = require('graphql');
-const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema } = graphql;
+const { GraphQLObjectType, GraphQLString, GraphQLInt, GraphQLSchema, GraphQLID } = graphql;   // id verisinin int yerine string ya da vice versa oldugunda ariza cikmasin diye GraphQLID kullaniliyor.
 const _ = require('lodash');
+
 const movies = [
 	{
 		id: '1',
@@ -22,14 +23,41 @@ const movies = [
 	}
 ];
 
+const directors = [
+	{
+		id: '1',
+		name: 'Francis Ford Coppola',
+		birth: 1939
+	},
+	{
+		id: '2',
+		name: 'Quentin Tarantino',
+		birth: 1963
+	},
+	{
+		id: '3',
+		name: 'Brian De Palma',
+		birth: 1940
+	}
+];
+
 
 const MovieType = new GraphQLObjectType({
   name: 'Movie',
   fields: {
-    id: { type: GraphQLString },
+    id: { type: GraphQLID },
     title: { type: GraphQLString },
     description: { type: GraphQLString },
     year: { type: GraphQLInt }
+  }
+});
+
+const DirectorType = new GraphQLObjectType({
+  name: 'Director',
+  fields: {
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    birth: { type: GraphQLInt }
   }
 });
 
@@ -39,9 +67,16 @@ const RootQuery = new GraphQLObjectType({
   fields: {
     movie: {
       type: MovieType,
-      args: { id: { type: GraphQLString } },   //movie objesini ararken neye gore arayacagimizi soyluyoruz ve parametre olarak veriyoruz
+      args: { id: { type: GraphQLID } },   //movie objesini ararken neye gore arayacagimizi soyluyoruz ve parametre olarak veriyoruz
       resolve(parentValue, args) {     //veriyi bize getirecek olan fonksiyon. Yarin bir gun bir filmin directorune erismek istersek de parentValue degiskenini kullanacagiz
         return _.find(movies, { id: args.id });
+      }
+    },
+    director: {
+      type: DirectorType,
+      args: { id: { type: GraphQLID } },
+      resolve(parentValue, args) {
+        return _.find(directors, { id: args.id });
       }
     }
   }
